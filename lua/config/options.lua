d8cbf2641d32ab787bf47341c83161097674d719
owo93 <Iztyle30@gmail.com>
2024-10-06 15:37:01 +0700

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,7 +16,6 @@ vim.opt.cmdheight = 1
 vim.opt.laststatus = 3
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
-vim.opt.shell = "pwsh.exe"
 
 if vim.fn.has("nvim-0.8") == 1 then
   vim.opt.cmdheight = 0
