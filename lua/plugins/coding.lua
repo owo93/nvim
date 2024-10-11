5be103fec9bb7147f3809756db6aa724386a1565
owo93 <Iztyle30@gmail.com>
2024-10-12 00:06:38 +0700

    refactor(coding): setup copilpot options for nvim-cmp

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -42,10 +42,31 @@ return {
   -- https://github.com/hrsh7th/nvim-cmp
   {
     "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+      {
+        "zbirenbaum/copilot-cmp",
+        dependencies = "copilot.lua",
+        opts = {},
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          copilot_cmp.setup(opts)
+          -- attach cmp source whenever copilot attaches
+          -- fixes lazy-loading issues with the copilot cmp source
+          LazyVim.lsp.on_attach(function(client)
+            copilot_cmp._on_insert_enter({})
+          end, "copilot")
+        end,
+      },
+    },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 2,
+        priority = 100,
+      })
     end,
   },
 }
