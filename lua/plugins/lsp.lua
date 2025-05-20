c21b4a94dff7548112e8ddf1463cdae60ae9e04d
owo93 <iztyle30@gmail.com>
2025-05-20 20:59:36 +0700

    :bug: (lsp) mason.nvim moved repo & disable lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,39 +1,39 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.toml" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.tex" },
-  { import = "lazyvim.plugins.extras.lang.rust" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.toml" },
+	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.lang.tex" },
+	{ import = "lazyvim.plugins.extras.lang.rust" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
 
-  -- https://github.com/williamboman/mason.nvim
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "stylua",
-        "selene",
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "css-lsp",
-        "rust-analyzer",
-      })
-    end,
-  },
+	-- https://github.com/mason-org/mason.nvim
+	{
+		"mason-org/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"stylua",
+				"luacheck",
+				"shellcheck",
+				"typescript-language-server",
+				"css-lsp",
+				"rust-analyzer",
+				"pyright",
+				"ruff",
+				"clangd",
+			})
+		end,
+	},
 
-  -- https://github.com/neovim/nvim-lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        rust_analyzer = {},
-      },
-    },
-  },
+	-- https://github.com/neovim/nvim-lspconfig
+	-- {
+	-- 	"neovim/nvim-lspconfig",
+	-- 	---@class PluginLspOpts
+	-- 	opts = {
+	-- 		---@type lspconfig.options
+	-- 		servers = {
+	-- 			rust_analyzer = {},
+	-- 		},
+	-- 	},
+	-- },
 }
