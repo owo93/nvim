7d563947cdffb01a1f3f2895c1a4acfd315239bd
owo93 <iztyle30@gmail.com>
2025-05-24 22:10:37 +0700

    :sparkles: add mini.align

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,48 +1,48 @@
 return {
-  -- https://github.com/stevearc/overseer.nvim
-  {
-    "stevearc/overseer.nvim",
-    cmd = {
-      "OverseerOpen",
-      "OverseerClose",
-      "OverseerToggle",
-      "OverseerSaveBundle",
-      "OverseerLoadBundle",
-      "OverseerDeleteBundle",
-      "OverseerRunCmd",
-      "OverseerRun",
-      "OverseerInfo",
-      "OverseerBuild",
-      "OverseerQuickAction",
-      "OverseerTaskAction",
-      "OverseerClearCache",
-    },
-    opts = {
-      dap = false,
-      task_list = {
-        bindings = {
-          ["<C-h>"] = false,
-          ["<C-j>"] = false,
-          ["<C-k>"] = false,
-          ["<C-l>"] = false,
-        },
-      },
-      form = {
-        win_opts = {
-          winblend = 0,
-        },
-      },
-      confirm = {
-        win_opts = {
-          winblend = 0,
-        },
-      },
-      task_win = {
-        win_opts = {
-          winblend = 0,
-        },
-      },
-    },
+	-- https://github.com/stevearc/overseer.nvim
+	{
+		"stevearc/overseer.nvim",
+		cmd = {
+			"OverseerOpen",
+			"OverseerClose",
+			"OverseerToggle",
+			"OverseerSaveBundle",
+			"OverseerLoadBundle",
+			"OverseerDeleteBundle",
+			"OverseerRunCmd",
+			"OverseerRun",
+			"OverseerInfo",
+			"OverseerBuild",
+			"OverseerQuickAction",
+			"OverseerTaskAction",
+			"OverseerClearCache",
+		},
+		opts = {
+			dap = false,
+			task_list = {
+				bindings = {
+					["<C-h>"] = false,
+					["<C-j>"] = false,
+					["<C-k>"] = false,
+					["<C-l>"] = false,
+				},
+			},
+			form = {
+				win_opts = {
+					winblend = 0,
+				},
+			},
+			confirm = {
+				win_opts = {
+					winblend = 0,
+				},
+			},
+			task_win = {
+				win_opts = {
+					winblend = 0,
+				},
+			},
+		},
   -- stylua: ignore
   keys = {
     { "<leader>ow", "<cmd>OverseerToggle<cr>",      desc = "Task list" },
@@ -53,39 +53,49 @@ return {
     { "<leader>ot", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" },
     { "<leader>oc", "<cmd>OverseerClearCache<cr>",  desc = "Clear cache" },
   },
-  },
+	},
 
-  --  https://github.com/saghen/blink.cmp
-  {
-    "saghen/blink.cmp",
-    dependencies = {
-      { "rafamadriz/friendly-snippets" },
-      { "giuxtaposition/blink-cmp-copilot" },
-    },
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      keymap = { preset = "enter" },
-      completion = {
-        menu = { border = "rounded" },
-        documentation = { window = { border = "rounded" } },
-      },
-      signature = {
-        enabled = true,
-        window = { border = "rounded" },
-      },
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer", "copilot" },
-        providers = {
-          copilot = {
-            name = "copilot",
-            module = "blink-cmp-copilot",
-            score_offset = 100,
-            async = true,
-          },
-        },
-      },
-    },
-    lazy = true,
-  },
+	--  https://github.com/saghen/blink.cmp
+	{
+		"saghen/blink.cmp",
+		dependencies = {
+			{ "rafamadriz/friendly-snippets" },
+			{ "giuxtaposition/blink-cmp-copilot" },
+		},
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = { preset = "enter" },
+			completion = {
+				menu = { border = "rounded" },
+				documentation = { window = { border = "rounded" } },
+			},
+			signature = {
+				enabled = true,
+				window = { border = "rounded" },
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "copilot" },
+				providers = {
+					copilot = {
+						name = "copilot",
+						module = "blink-cmp-copilot",
+						score_offset = 100,
+						async = true,
+					},
+				},
+			},
+		},
+		lazy = true,
+	},
+
+	-- https://github.com/echasnovski/mini.align
+	{
+		"echasnovski/mini.align",
+		event = "BufReadPost",
+		config = function()
+			local align = require("mini.align")
+			align.setup()
+		end,
+	},
 }
