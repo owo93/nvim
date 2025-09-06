e8dd9441e831abeecab9e6d14f0c76bc419fa7a8
owo93 <iztyle30@gmail.com>
2025-09-06 10:48:55 +0700

    :sparkles: properly install typst-preview

--- lua/plugins/research.lua
+++ lua/plugins/research.lua
@@ -1,20 +1,27 @@
 return {
-  -- add tinymist to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "chomosuke/typst-preview.nvim",
-      "Myriad-Dreamin/tinymist",
-    },
-    opts = {
-      servers = {
-        tinymist = {
-          settings = {
-            exportPdf = "onType",
-            outputPath = "$root/target/$dir/$name",
-          },
-        },
-      },
-    },
-  },
+	{
+		"chomosuke/typst-preview.nvim",
+		lazy = false,
+		version = "1.*",
+		opts = {},
+	},
+
+	-- add tinymist to lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"chomosuke/typst-preview.nvim",
+			"Myriad-Dreamin/tinymist",
+		},
+		opts = {
+			servers = {
+				tinymist = {
+					settings = {
+						exportPdf = "onType",
+						outputPath = "$dir/target/$name",
+					},
+				},
+			},
+		},
+	},
 }
