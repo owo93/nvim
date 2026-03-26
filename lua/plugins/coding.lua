3a7538bf9e6242e353b45b2622007732ff511325
owo93 <Iztyle30@gmail.com>
2026-03-26 17:28:12 +0700

    chore: stylua .

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,23 +1,23 @@
 return {
-	-- https://github.com/zbirenbaum/copilot.lua
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		build = ":Copilot auth",
-		opts = function(_, opts)
-			opts.suggestion = {
-				enabled = false,
-				auto_trigger = false,
-				debounce = 75,
-			}
-			opts.panel = {
-				enabled = false,
-			}
-			opts.filetypes = {
-				markdown = true,
-				help = true,
-				["."] = false,
-			}
-		end,
-	},
+  -- https://github.com/zbirenbaum/copilot.lua
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = function(_, opts)
+      opts.suggestion = {
+        enabled = false,
+        auto_trigger = false,
+        debounce = 75,
+      }
+      opts.panel = {
+        enabled = false,
+      }
+      opts.filetypes = {
+        markdown = true,
+        help = true,
+        ["."] = false,
+      }
+    end,
+  },
 }
--- lua/plugins/research.lua
+++ lua/plugins/research.lua
@@ -1,38 +1,37 @@
 return {
-	{
-		"chomosuke/typst-preview.nvim",
-		lazy = false,
-		ft = "typst",
-		version = "1.*",
-		opts = {
-			port = 3000,
-			open_cmd = "start %s",
-			invert_colors = "auto",
-		},
-	},
+  {
+    "chomosuke/typst-preview.nvim",
+    lazy = false,
+    ft = "typst",
+    version = "1.*",
+    opts = {
+      port = 3000,
+      invert_colors = "auto",
+    },
+  },
 
-	-- add tinymist to lspconfig
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"chomosuke/typst-preview.nvim",
-			"Myriad-Dreamin/tinymist",
-		},
-		opts = {
-			servers = {
-				tinymist = {
-					settings = {
-						formatterMode = "typstyle",
-						semanticTokens = "enable",
-					},
-				},
-			},
-		},
-	},
+  -- add tinymist to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "chomosuke/typst-preview.nvim",
+      "Myriad-Dreamin/tinymist",
+    },
+    opts = {
+      servers = {
+        tinymist = {
+          settings = {
+            formatterMode = "typstyle",
+            semanticTokens = "enable",
+          },
+        },
+      },
+    },
+  },
 
-	-- https://github.com/larvag/vimtex
-	{
-		"lervag/vimtex",
-		lazy = false,
-	},
+  -- https://github.com/larvag/vimtex
+  {
+    "lervag/vimtex",
+    lazy = false,
+  },
 }
